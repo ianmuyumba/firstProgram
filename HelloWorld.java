f31566d78ee849c7359ef853355868eddb5102a9
Ian Muyumba <ianmandela@gmail.com>
2026-09-17 14:35:20 +0300

    new file:   HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+* My first Java program
+* @author Your Name
+* @date Today's Date
+*/
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Hello, World!");
+System.out.println("My name is: Ian");
+System.out.println("I am learning Java at Riara University");
+System.out.println("Today's date is: Thursday, September 17, 2026");
+}
+}
